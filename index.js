cc4cf535e76365d0ee4ed421806d90fde1544fa5
Vinícius Ramos Jacob <vini_jacob@hotmail.com>
2022-09-15 16:29:05 -0400

    new design, still porr

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var btn = document.querySelector('#eye_btn');
 var change = document.querySelector('#user_password');
+var valor = change.value;
 var state = true;
 
 btn.addEventListener('click', () => {
     console.log('password functional!');
+    console.log(valor);
     
     if (state) {
         change.setAttribute('type', 'text');
